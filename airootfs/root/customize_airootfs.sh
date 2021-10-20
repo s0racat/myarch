665b3d709839dc017c67b6d267d182cb9810acdb
soracqt <72482919+soracqt@users.noreply.github.com>
2021-10-20 09:57:26 +0900

    try ci

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -29,4 +29,3 @@ rm -rf /etc/systemd/system/{choose-mirror.service,pacman-init.service,etc-pacman
 rm -f /root/{.automated_script.sh,.zlogin}
 rm -f /etc/mkinitcpio-archiso.conf
 rm -rf /etc/initcpio
-rm -fv /etc/resolv.conf
